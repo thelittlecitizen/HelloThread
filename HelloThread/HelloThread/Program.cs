c637fdc6d2dcd1777ebde0218361d9f79b1dfcef
thelittlecitizen <thelittlecitizen1@gmail.com>
2020-08-24 22:55:50 +0300

    Finished

--- HelloThread/HelloThread/Program.cs
+++ HelloThread/HelloThread/Program.cs
@@ -1,27 +1,64 @@
 ﻿using System;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace HelloThread
 {
     class Program
     {
+        public static int count;
+        public static int countEmpire;
+        public static int countMamas;
+        public static object locker = new object();
+
         static void Main(string[] args)
         {
-            Thread thread1 = new Thread(() => Print("Mamas"));
-            thread1.IsBackground = true;
-            thread1.Start();
-            Thread thread2 = new Thread(() => Print("Empire"));
-            thread2.IsBackground = true;
-            thread2.Start();
-        
-        static void Print(string word)
+            Thread mamas = new Thread(() => PrintCount());
+            mamas.IsBackground = false;
+            mamas.Start();
+
+            Thread empire = new Thread(() => PrintCount());
+            empire.IsBackground = false;
+            empire.Start();
+
+
+            mamas.Join();
+            empire.Join();
+
+            Console.WriteLine($"mamas {countMamas} and empire {countEmpire}");
+            
+        }
+        /*static void Print(string word)
+        {
+            for (int i = 0; i < 10000; i++)
+                Console.WriteLine(word);
+        }*/
+        static void PrintCount()
+        {
+            while (count < 10000)
             {
-                for (int i=0; i <1000; i++)
-                    Console.WriteLine(word);
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine("Empire");
+                   // count++;
+                    countEmpire++;
+                }
+
+                if (count % 2 == 0)
+                {
+                    Console.WriteLine("Mamas");
+                   // count++;
+                    countMamas++;
+                }
+                lock (locker)
+                {
+                    count++;
+
+                }
             }
-                
 
         }
-        
+
+
     }
 }
