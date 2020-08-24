a28b087503c7029282e7b79b0ef476dc5a523933
thelittlecitizen <thelittlecitizen1@gmail.com>
2020-08-24 21:16:20 +0300

    Create threads as backfrond

---
+++ HelloThread/HelloThread/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread thread1 = new Thread(() => Print("Mamas"));
+            thread1.IsBackground = true;
+            thread1.Start();
+            Thread thread2 = new Thread(() => Print("Empire"));
+            thread2.IsBackground = true;
+            thread2.Start();
+        
+        static void Print(string word)
+            {
+                for (int i=0; i <1000; i++)
+                    Console.WriteLine(word);
+            }
+                
+
+        }
+        
+    }
+}
